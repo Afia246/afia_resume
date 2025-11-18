d9ba01f3b8a5ea9bdaf863eeac68cfe5af58a016
Afia246 <139486810+Afia246@users.noreply.github.com>
2025-11-18 17:10:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function reveal() {
+    let elements = document.querySelectorAll('.reveal');
+
+    elements.forEach(el => {
+        let windowHeight = window.innerHeight;
+        let elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', reveal);
+reveal();
